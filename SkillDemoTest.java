ddcf365911fc09ceefaaea90e24d903483b197e4
kl2024 <k1luong@ucsd.edu>
2022-05-11 23:50:12 -0700

    added test files
    Created SkillDemoTest.java and method for SkillDemo.java

---
+++ SkillDemoTest.java
@@ -0,0 +1,11 @@
+import java.beans.Transient;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    //here, we add a test for our subtract function
+    @Test
+    public void subtraction() {
+        assertEquals(0, SkillDemo.subtract(2,1));
+    }
+}
